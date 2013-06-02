e4284a4a76e826cb3666d62b6db9ebcbb96436e5
Mark Klara <mrhappyasthma@gmail.com>
2013-06-02 13:43:53 -0400

    Initial Commit

---
+++ SpaceViking/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  SpaceViking
+//
+//  Created by MrHappyAsthma on 6/2/13.
+//  Copyright MrHappyAsthma 2013. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
---
+++ SpaceViking/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SpaceViking
+//
+//  Created by MrHappyAsthma on 6/2/13.
+//  Copyright MrHappyAsthma 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
