641672096d90c8b403186382d2bb0f7513ed8391
Mark Klara <mrhappyasthma@gmail.com>
2013-06-04 20:16:53 -0400

    Game Objects Completed

--- SpaceViking/Classes/GameObjects/GameCharacter.h
+++ SpaceViking/Classes/GameObjects/GameCharacter.h
@@ -6,8 +6,19 @@
 //  Copyright (c) 2013 MrHappyAsthma. All rights reserved.
 //
 
+#import <Foundation/Foundation.h>
 #import "GameObject.h"
 
 @interface GameCharacter : GameObject
+{
+    int characterHealth;
+    CharacterStates characterState;
+}
+
+-(void)checkAndClampSpritePosition;
+-(int)getWeaponDamage;
+
+@property (readwrite) int characterHealth;
+@property (readwrite) CharacterStates characterState;
 
 @end
--- SpaceViking/Classes/GameObjects/GameCharacter.m
+++ SpaceViking/Classes/GameObjects/GameCharacter.m
@@ -9,5 +9,45 @@
 #import "GameCharacter.h"
 
 @implementation GameCharacter
+@synthesize characterHealth;
+@synthesize characterState;
+
+-(int)getWeaponDamage
+{
+    // Default to zero damage
+    CCLOG(@"getWeaponDamage should be overridden");
+    return 0;
+}
+
+-(void)checkAndClampSpritePosition
+{
+    CGPoint currentSpritePosition = [self position];
+    
+    //Remember that Cocos2D uses a point system to help position objects on the normal iPhone display (480 × 320 pixels) and the retina display (960 × 640 pixels). In the nonretina display a point equals a pixel, but in the retina display a point equals two pixels.
+    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad)
+    {
+        // Clamp for the iPad
+        if (currentSpritePosition.x < 30.0f)
+        {
+            [self setPosition:ccp(30.0f, currentSpritePosition.y)];
+        }
+        else if (currentSpritePosition.x > 1000.0f)
+        {
+            [self setPosition:ccp(1000.0f, currentSpritePosition.y)];
+        }
+    }
+    else
+    {
+        // Clamp for iPhone, iPhone 4, or iPod touch
+        if (currentSpritePosition.x < 24.0f)
+        {
+            [self setPosition:ccp(24.0f, currentSpritePosition.y)];
+        }
+        else if (currentSpritePosition.x > 456.0f)
+        {
+            [self setPosition:ccp(456.0f, currentSpritePosition.y)];
+        }
+    }
+}
 
 @end
